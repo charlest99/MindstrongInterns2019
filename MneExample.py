824dd13a17642d6e2b150b9b9eb32bd136558766
Charles Tananbaum <charlestananbaum@gmail.com>
2019-06-10 13:51:26 -0700

    Create MneExample.py

---
+++ MneExample.py
@@ -0,0 +1,81 @@
+#import statements
+import os
+import numpy as np
+import mne
+
+
+#import data
+sample_data_folder = mne.datasets.sample.data_path()
+sample_data_raw_file = os.path.join(sample_data_folder, 'MEG', 'sample',
+                                    'sample_audvis_filt-0-40_raw.fif')
+raw = mne.io.read_raw_fif(sample_data_raw_file)
+
+#print info about the fif dataset
+print(raw)
+print(raw.info)
+
+raw.plot_psd(fmax=50)
+raw.plot(duration=5, n_channels=30)
+
+ica = mne.preprocessing.ICA(n_components=20, random_state=97, max_iter=800)
+ica.fit(raw)
+ica.exclude = [1, 2]  # details on how we picked these are omitted here
+ica.plot_properties(raw, picks=ica.exclude)
+
+orig_raw = raw.copy()
+raw.load_data()
+ica.apply(raw)
+
+# show some frontal channels to clearly illustrate the artifact removal
+chs = ['MEG 0111', 'MEG 0121', 'MEG 0131', 'MEG 0211', 'MEG 0221', 'MEG 0231',
+       'MEG 0311', 'MEG 0321', 'MEG 0331', 'MEG 1511', 'MEG 1521', 'MEG 1531',
+       'EEG 001', 'EEG 002', 'EEG 003', 'EEG 004', 'EEG 005', 'EEG 006',
+       'EEG 007', 'EEG 008']
+chan_idxs = [raw.ch_names.index(ch) for ch in chs]
+orig_raw.plot(order=chan_idxs, start=12, duration=4)
+raw.plot(order=chan_idxs, start=12, duration=4)
+
+events = mne.find_events(raw, stim_channel='STI 014')
+print(events[:5])  # show the first 5
+
+event_dict = {'auditory/left': 1, 'auditory/right': 2, 'visual/left': 3,
+              'visual/right': 4, 'smiley': 5, 'buttonpress': 32}
+              
+fig = mne.viz.plot_events(events, event_id=event_dict, sfreq=raw.info['sfreq'])
+fig.subplots_adjust(right=0.7)  # make room for the legend
+
+reject_criteria = dict(mag=4000e-15,     # 4000 fT
+                       grad=4000e-13,    # 4000 fT/cm
+                       eeg=150e-6,       # 150 μV
+                       eog=250e-6)       # 250 μV
+
+epochs = mne.Epochs(raw, events, event_id=event_dict, tmin=-0.2, tmax=0.5,
+                    reject=reject_criteria, preload=True)
+                    
+                    conds_we_care_about = ['auditory/left', 'auditory/right',
+                       'visual/left', 'visual/right']
+                       
+epochs.equalize_event_counts(conds_we_care_about)  # this operates in-place
+aud_epochs = epochs['auditory']
+vis_epochs = epochs['visual']
+del raw, epochs  # free up memory
+
+aud_epochs.plot_image(picks=['MEG 1332', 'EEG 021'])
+
+frequencies = np.arange(7, 30, 3)
+power = mne.time_frequency.tfr_morlet(aud_epochs, n_cycles=2, return_itc=False,
+                                      freqs=frequencies, decim=3)
+power.plot(['MEG 1332'])
+
+aud_evoked = aud_epochs.average()
+vis_evoked = vis_epochs.average()
+
+mne.viz.plot_compare_evokeds(dict(auditory=aud_evoked, visual=vis_evoked),
+                             show_legend='upper left',
+                             show_sensors='upper right')
+                             
+aud_evoked.plot_joint(picks='eeg')
+aud_evoked.plot_topomap(times=[0., 0.08, 0.1, 0.12, 0.2], ch_type='eeg')
+
+
+
