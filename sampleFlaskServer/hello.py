9966e91a4f7c31b90c03d6f6573d66b3a7d51e2e
adrohan19 <48809507+adrohan19@users.noreply.github.com>
2019-06-27 16:05:21 -0700

    updated version

--- sampleFlaskServer/hello.py
+++ sampleFlaskServer/hello.py
@@ -1,15 +1,21 @@
 from flask import Flask, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
-def main():
-    return render_template('main.html')
+def index():
+    return render_template('index.html')
 
-@app.route('/contrast_img/')
-def contrast():
-	return render_template('hicontrast.html')
+@app.route('/hicontrast', methods=['GET', 'POST'])
+def hicontrast():
+    return render_template('hicontrast.html')
 
+
+@app.route('/tapping', methods=['GET', 'POST'])
+def tapping():
+    return render_template('tapping.html')
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
